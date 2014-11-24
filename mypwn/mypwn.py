a918a6ddac781b543af6b3cdff69a49c2d31a888
ddaa <chengtc@cs.nctu.edu.tw>
2014-11-24 19:22:29 +0800

    1. add length to send() 2. add class Libc_func to count libc easier

--- mypwn/mypwn.py
+++ mypwn/mypwn.py
@@ -1,8 +1,8 @@
 import telnetlib
 import socket
 
-host = ""
-port = -1
+host = "localhost"
+port = 55666
 
 t = telnetlib.Telnet(host, port)
 s = t.get_socket()
@@ -16,7 +16,7 @@ def connect(host, port):
     t = telnetlib.Telnet(host, port)
     s = t.get_socket()
 
-def read_until(end)
+def read_until(end):
     buf = ""
     while True:
         buf += s.recv(1)
@@ -28,7 +28,9 @@ def read_until(end)
 def read_line():
     return read_until("\n")
 
-def send(buf):
+def send(buf, length = 0):
+    for i in range(length - len(buf)):
+        buf += "\x00"
     s.send(buf)
 
 def send_line(buf):
@@ -44,12 +46,47 @@ def pack(addr):
 def unpack(addr):
     return addr.decode("hex")[::-1]
 
-def get_libcfunc(remote_addr, src_offset, dest_offset):
-    libc_base = int(remote_addr, 16) - src_offset
-    return hex(libc_base + dest_offset)[2:]
-
 def int_to_dword(num):
     if num > 0:
         return hex(num)
     else:
         return hex(pow(2, 32) + num)
+
+#*************************#
+#     count libc base     #
+#*************************#
+
+class Libc_func:
+    path = ""
+    libc_base = -1
+    func = {"write" :"__write",\
+            "read" : "__read",\
+            "system" : "__libc_system",\
+            "open" : "__open",\
+            "gets" : "_IO_gets"}
+
+    def __init__(self, path):
+        self.path = path
+    
+    def count_libcbase(self, f, addr):
+        assert self.path != "", "plz set libc path first!"
+        import subprocess
+        cmd = "readelf -s " + self.path + "| grep " + self.func[f] + "@@" + " | awk '{print $2}'"
+        p = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
+        offset = int(p.stdout.read().strip(), 16)
+        self.libc_base = int(addr, 16) - offset
+        print "libc_base : " + hex(self.libc_base)
+
+        # count d
+
+    def libc(self, f):
+        assert self.libc_base != -1, "plz count libc base first!!"
+        import subprocess
+        cmd = "readelf -s " + self.path + "| grep " + self.func[f] + "@@" + " | awk '{print $2}'"
+        p = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
+        offset = int(p.stdout.read().strip(), 16)
+        addr = hex(self.libc_base + offset)
+        print f + " : " + addr 
+        return addr[2:]
+
+       
