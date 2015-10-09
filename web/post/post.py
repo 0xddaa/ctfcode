fa0b0f1958d5262c4224b158403a3c51365a37c9
ddaa <0xddaa@gmail.com>
2015-10-09 15:01:33 +0800

    rewrite post.py by request

--- web/post/post.py
+++ web/post/post.py
@@ -1,21 +1,29 @@
-#!/usr/bin/python
-#coding=utf-8
+#!/usr/bin/env python
 
-import urllib
-import urllib2
+import requests
 
-def post(url, data):
-	req = urllib2.Request(url)
-	data = urllib.urlencode(data)
-	#enable cookie
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-	response = opener.open(req, data)
-	return response.read()
+post = requests.post
+get = requests.get
 
-def main():
-	posturl = "http://ctf.tw:6003/admin.php"
-	data = {'user':'asd\'#', 'password':'', 'submit':'Login'}
-	print post(posturl, data)
+def login(uri, user, passwd):
+    r = get(uri)
+    start = r.text.find("authenticity_token") + 56
+    end = start + r.text[start:].find("\"")
+    csrf = r.text[start:end]
+    post_data = {"user[username]": user, "user[password]": passwd, "authenticity_token": csrf}
+    r = post(uri, data = post_data, cookies = r.cookies)
+    return r.cookies
 
-if __name__ == '__main__':
-	main()
+def submit_flag(uri, cookies, flag, fail_pattern = "Your flag is wrong! :("):
+    r = get("http://140.113.194.85:81/problems/1", cookies = cookies)
+    start = r.text.find("authenticity_token") + 56
+    end = start + r.text[start:].find("\"")
+    csrf = r.text[start:end]
+    post_data = {"authenticity_token": csrf, \
+        "problem_id": "1", \
+        "flag": flag}
+    r = requests.post("http://140.113.194.85:81/submit", data = post_data, cookies = cookies)
+    return fail_pattern not in r.text
+
+cookies = login("http://140.113.194.85:81/users/sign_in", "meheap", "12345678")
+submit_flag("http://140.113.194.85:81/submit", cookies, flag):
