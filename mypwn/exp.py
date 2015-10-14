ee843178b597079b182e083d23031d9b526c24a1
ddaa <0xddaa@gmail.com>
2015-10-14 18:24:48 +0800

    modify template for new version pwntools

--- mypwn/exp.py
+++ mypwn/exp.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python
 
 from pwn import *
-from pwnlib.log import *
-
-def pad(size, buf=''):
-    chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    buflen = size - len(buf)
-    assert buflen >= 0, "%d bytes over" % (-buflen,)
-    return ''.join(random.choice(chars) for i in xrange(buflen))
+info = log.info
 
 # setting 
-elf = ELF("bamboobox")
+elf = ELF("")
 libc = ELF("libc.so.6")
 
 # trigger vuln
