8f1ed3d8bbad38192f1a02cafff4168d84bf60e8
0xddaa <0xddaa@gmail.com>
2019-09-24 09:27:40 +0800

    docker_tpl: allow to select tag

--- docker_tpl/build.sh
+++ docker_tpl/build.sh
@@ -3,13 +3,15 @@
 workdir="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ -z "$1" ]; then
-    echo 'Usage: ./build.sh [binary_path]'
+    echo 'Usage: ./build.sh [binary_path] <tags>'
     exit 1
 fi
 
 problem=$(basename "$1")
+[ -z "$2" ] && tag=latest || tag="$2"
 
 find "$workdir" -type f -exec sed -i "s/PROBLEM/$problem/g" {} \;
+sed -i "s/TAG/$tag/g" "$workdir/Dockerfile"
 echo "FLAG{this_is_the_flag_for_$problem}" > "$workdir/share/flag"
 cp "$1" "$workdir/share/"
 chmod u+x "$workdir/share/$problem"
